5f616a7b0edc39763282a0ddd4448db4be8dea3d
Joel Sousa <joelbtsousa@gmail.com>
2024-02-25 17:13:43 +0000

    Initial commit for the project

---
+++ src/main/java/com/Natixis/NX/BancoNatixisApplication.java
@@ -0,0 +1,13 @@
+package com.Natixis.NX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoNatixisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoNatixisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Natixis/NX/BancoNatixisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Natixis.NX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoNatixisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
